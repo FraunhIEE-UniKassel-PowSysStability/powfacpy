7a426976810515cd1056aa2120ffb82ad6f33902
Simon <simon.eberlein@gmx.de>
2022-09-05 14:17:45 +0200

    docs

---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'powfacpy'
+copyright = '2022, Fraunhofer IEE'
+author = 'Fraunhofer IEE'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_context = {
+  'display_github': True,
+  'github_user': 'Sciemon',
+  'github_repo': 'powfacpy',
+  'github_version': 'main/docs/source/',
+}
---
+++ docs/source/tutorials.rst
@@ -0,0 +1,23 @@
+.. _tutorials:
+
+Tutorials
+=========
+
+The tutorials are Jupyter notebooks which you can find `here <https://github.com/Sciemon/powerfactorypy/tree/main/tutorials>`_
+or click on the links below to go to a specific tutorial.
+
+Getting started
+----------------
+Start here with the 
+`getting started tutorial <https://github.com/Sciemon/powerfactorypy/blob/main/tutorials/getting_started.ipynb>`_.
+This tutorial gives an overview of the interaction with the *PowerFactory* 
+database using *powfacpy*. The methods introduced in this tutorial serve as a basis to 
+do more complex things and to write more readable code using less lines of code.
+
+Dynamic Simulation
+------------------
+ToDo
+
+Plotting
+--------------
+ToDo
