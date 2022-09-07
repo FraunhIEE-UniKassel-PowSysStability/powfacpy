817d853219d6c777378963a87662e23d05598f64
Simon <simon.eberlein@gmx.de>
2022-09-07 10:11:04 +0200

    Update paths to PF objects in plot tests

--- tests/test_plot_interface.py
+++ tests/test_plot_interface.py
@@ -23,14 +23,14 @@ def test_plot(pfplot,activate_test_project):
     pfsim.initialize_and_run_sim()
 
     with pytest.raises(powfacpy.PFNoPlotActivatedError):
-        pfplot.plot(r"Network Model\Network Data\Grid\AC Voltage Source",["s:u0","m:Qsum:bus1"])
+        pfplot.plot(r"Network Model\Network Data\test_plot_interface\Grid 1\AC Voltage Source",["s:u0","m:Qsum:bus1"])
 
     pfplot.clear_plot_pages()
     pfplot.set_active_plot("test_plot 1","test_plot_interface 1")
-    pfplot.plot(r"Network Model\Network Data\Grid\AC Voltage Source",["s:u0","m:Qsum:bus1"])
+    pfplot.plot(r"Network Model\Network Data\test_plot_interface\Grid 1\AC Voltage Source",["s:u0","m:Qsum:bus1"])
 
     pfplot.set_active_plot("test_plot 2","test_plot_interface 1")
-    pfplot.plot(r"Network Model\Network Data\Grid\AC Voltage Source","s:u0",
+    pfplot.plot(r"Network Model\Network Data\test_plot_interface\Grid 1\AC Voltage Source","s:u0",
         linestyle=2,linewidth=100,color=3,label="u0 test",
         result_obj=r"Study Cases\test_plot_interface\Study Case 1\All calculations")
 
@@ -44,10 +44,10 @@ def test_pyplot_from_csv(pfplot,activate_test_project):
     pfsim.export_to_csv(file_name=file_name)
 
     pyplot.figure()
-    powfacpy.PFPlotInterface.pyplot_from_csv(
+    powfacpy.PFPlotInterface.plot_from_csv(
         export_dir + "\\" + file_name + ".csv",
-        [r"Network Model\Network Data\Grid\AC Voltage Source\s:u0",
-        r"Network Model\Network Data\Grid\AC Voltage Source\m:Qsum:bus1"]) 
+        [r"Network Model\Network Data\test_plot_interface\Grid 1\AC Voltage Source\s:u0",
+        r"Network Model\Network Data\test_plot_interface\Grid 1\AC Voltage Source\m:Qsum:bus1"]) 
     pyplot.xlabel("t [s]")
 
 def test_copy_graphics_board_content(pfplot,activate_test_project):
@@ -55,7 +55,10 @@ def test_copy_graphics_board_content(pfplot,activate_test_project):
     target_study_cases = [r"Study Cases\test_plot_interface\Study Case 2", 
         r"Study Cases\test_plot_interface\Study Case 3"]
     pfplot.copy_graphics_board_content(source_study_case,target_study_cases,
-        obj_to_copy="*.GrpPage",clear_target_graphics_board=True)  
+        obj_to_copy="*.GrpPage",clear_target_graphics_board=True) 
+
+    pfplot.copy_graphics_board_content(source_study_case,r"Study Cases\test_plot_interface\Study Case 2",
+        obj_to_copy="*.GrpPage",clear_target_graphics_board=True)      
 
 def test_copy_graphics_board_content_to_all_study_cases(pfplot,activate_test_project):
     source_study_case = r"Study Cases\test_plot_interface\Study Case 1"
