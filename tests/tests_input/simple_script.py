bebd55c6c7db17777b2e73791b2efea15043fda8
Simon <simon.eberlein@gmx.de>
2023-02-10 16:02:44 +0100

    Add folders for input and output files of unit tests to better structure the tests folder

---
+++ tests/tests_input/simple_script.py
@@ -0,0 +1,5 @@
+import powerfactory
+
+app = powerfactory.GetApplication()
+
+app.PrintPlain("This is a simple embedded test script.")
---
+++ tests/tests_output/file_from_embedded_script.py
@@ -0,0 +1,21 @@
+import powerfactory
+
+app = powerfactory.GetApplication()
+
+app.PrintPlain("This is an embedded test script.")
+
+#### Insert after this line
+
+# This should be replaced
+
+#### Insert before this line
+
+
+
+#### Insert powfacpy after this line
+
+# This should be replaced
+
+#### End powfacpy before this line
+
+
