6860d4f8f52e776592b7e66388e58addd9976858
Martin <franke_martin@gmx.net>
2023-03-28 11:52:59 +0200

    fix test_base_interface

--- tests/test_results_interface.py
+++ tests/test_results_interface.py
@@ -5,7 +5,7 @@ import powfacpy
 import importlib
 importlib.reload(powfacpy)
 
-from test_base_interface import pfbi, pf_app, activate_test_project_quasi_dynamic
+from test_base_interface import pf_app, activate_test_project_quasi_dynamic, pfbi
 
 @pytest.fixture
 def pfri(pf_app):
@@ -14,11 +14,11 @@ def pfri(pf_app):
     return pfri
 
 def test_export_to_pandas(pfri, activate_test_project_quasi_dynamic):
-    object = pfbi.get_obj(r'Network Model\Network Data\test_base_interface\Grid\General Load HV.ElmLod', include_subfolders=True)[0]
+    object = pfri.get_obj(r'Network Model\Network Data\test_base_interface\Grid\General Load HV.ElmLod', include_subfolders=True)[0]
     variables = ['m:i1:bus1', 'm:u1:bus1']
     nr_of_columns_including_time = len(variables) + 1
-    elmres = pfbi.app.GetFromStudyCase('ElmRes')
-    df = pfbi.export_to_pandas(
+    elmres = pfri.app.GetFromStudyCase('ElmRes')
+    df = pfri.export_to_pandas(
         result_objects=[elmres,]*len(variables),
         elements=[object,]*len(variables), 
         variables=variables)
