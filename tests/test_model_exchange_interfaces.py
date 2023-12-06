c41f2ca25ec288ed17deaf9a0a8fe699c543ada3
Martin <franke_martin@gmx.net>
2023-12-06 17:19:51 +0100

    refactor tests and add ssh update test

--- tests/test_model_exchange_interfaces.py
+++ tests/test_model_exchange_interfaces.py
@@ -6,14 +6,56 @@ import importlib
 importlib.reload(powfacpy)
 import os
 import glob
-
+import numpy.random as random
 from test_base_interface import pfbi, pf_app, activate_test_project
+import numpy as np
 
 @pytest.fixture
 def pfcgmes(pf_app):
     # Return PFModelExchangeInterface instance and show app
     pfcgmes = powfacpy.PFCgmesInterface(pf_app)
     return pfcgmes
+
+
+def _get_loadflow_results(pfcgmes):
+    loadflow = pfcgmes.app.GetFromStudyCase('ComLdf')
+    loadflow.Execute()
+    terminals = pfcgmes.app.GetCalcRelevantObjects('*.ElmTerm')
+    voltages = {term.loc_name:term.GetAttribute('m:u1') for term in terminals}
+    lines = pfcgmes.app.GetCalcRelevantObjects('*.ElmLne')
+    active_powers = {line.loc_name:line.GetAttribute('m:P:bus1') for line in lines}
+    return voltages, active_powers
+
+
+def _clear_output_path(output_path):
+    _ = [os.remove(file) for file in glob.glob(os.path.join(output_path, '*'))]
+    return None
+
+
+def _get_output_path():
+    return os.path.abspath(r'.\tests\tests_output\cgmes_export')
+
+
+def _activate_new_study_case_and_grid(pfcgmes):
+    pfcgmes.app.GetActiveStudyCase().Deactivate()
+    new_study_case = pfcgmes.create_in_folder(r'Study Cases\test_model_exchange_interfaces', 'New Study Case.IntCase', overwrite=True)
+    new_study_case.Activate()
+    new_grid = pfcgmes.create_in_folder(r'Network Model\Network Data\test_model_exchange_interfaces', 'New Grid.ElmNet', overwrite=True)
+    new_grid.Activate()
+    return new_study_case
+
+
+def _compare_loadflow_results(before:dict, after:dict, THRESHOLD=1e-4):
+    for element_name, value_before in before.items():
+        before_after_difference = abs((value_before - after[element_name])/value_before)
+        assert before_after_difference < THRESHOLD
+    pass
+
+
+def _assert_that_steady_state_changed(u1:dict, u2:dict):
+    u1 = np.array(list(u1.values()))
+    u2 = np.array(list(u2.values()))
+    assert np.abs((u1-u2)/u1).max() > 0.01
 
 
 def test_cgmes_export(pfcgmes, activate_test_project):
@@ -40,48 +82,55 @@ def test_cgmes_export(pfcgmes, activate_test_project):
 
 
 def test_cgmes_export_import(pfcgmes, activate_test_project):
-    OUTPUT_PATH = os.path.abspath(r'.\tests\tests_output\cgmes_export')
-    clear_output_path = lambda: [os.remove(file) for file in glob.glob(os.path.join(OUTPUT_PATH, '*'))]
-    clear_output_path()
+    OUTPUT_PATH = _get_output_path()
+    _clear_output_path(OUTPUT_PATH)
     
     pfcgmes.get_unique_obj(r"Study Cases\test_model_exchange_interfaces\Study Case").Activate()
-    
-    loadflow_before = pfcgmes.app.GetFromStudyCase('ComLdf')
-    loadflow_before.Execute()
-    terminals = pfcgmes.app.GetCalcRelevantObjects('*.ElmTerm')
-    voltages_before = {term.loc_name:term.GetAttribute('m:u1') for term in terminals}
-    lines = pfcgmes.app.GetCalcRelevantObjects('*.ElmLne')
-    active_powers_before = {line.loc_name:line.GetAttribute('m:P:bus1') for line in lines}
+    voltages_before, active_powers_before = _get_loadflow_results(pfcgmes)
 
     pfcgmes.cgmes_export(OUTPUT_PATH, selected_profiles='all', as_zip=True)
 
-    pfcgmes.app.GetActiveStudyCase().Deactivate()
-    new_study_case = pfcgmes.create_in_folder(r'Study Cases\test_model_exchange_interfaces', 'New Study Case.IntCase')
-    new_study_case.Activate()
-    new_grid = pfcgmes.create_in_folder(r'Network Model\Network Data\test_model_exchange_interfaces', 'New Grid.ElmNet')
-    new_grid.Activate()
-
+    _ = _activate_new_study_case_and_grid(pfcgmes)
     pfcgmes.cgmes_import(OUTPUT_PATH + '\\' + pfcgmes.EXPORTED_ZIP_NAME + '.zip')
 
-    loadflow_after = pfcgmes.app.GetFromStudyCase('ComLdf')
-    loadflow_after.Execute()
-    terminals = pfcgmes.app.GetCalcRelevantObjects('*.ElmTerm')
-    voltages_after = {term.loc_name:term.GetAttribute('m:u1') for term in terminals}
-    lines = pfcgmes.app.GetCalcRelevantObjects('*.ElmLne')
-    active_powers_after = {line.loc_name:line.GetAttribute('m:P:bus1') for line in lines}
-    
-    for terminal_name, voltage_before in voltages_before.items():
-        before_after_difference = abs(voltage_before - voltages_after[terminal_name])
-        assert before_after_difference < 1e-4
-    for line_name, active_power_before in active_powers_before.items():
-        before_after_difference = abs(active_power_before - active_powers_after[line_name])
-        assert before_after_difference < 1e-2
+    voltages_after, active_powers_after = _get_loadflow_results(pfcgmes)
+
+    _compare_loadflow_results(voltages_before, voltages_after)
+    _compare_loadflow_results(active_powers_before, active_powers_after)
 
 
 def test_cgmes_update_profiles(pfcgmes, activate_test_project):
-    #TODO
-    pass
+    OUTPUT_PATH = _get_output_path()
+    _clear_output_path(OUTPUT_PATH)
+    
+    study_case_source = pfcgmes.get_unique_obj(r"Study Cases\test_model_exchange_interfaces\Study Case")
+    study_case_source.Activate()
+    pfcgmes.cgmes_export(OUTPUT_PATH, selected_profiles='all', as_zip=True)
 
+    study_case_destination = _activate_new_study_case_and_grid(pfcgmes)
+    pfcgmes.cgmes_import(OUTPUT_PATH + '\\' + pfcgmes.EXPORTED_ZIP_NAME + '.zip')
+
+    voltages_source_before_changes, active_powers_source_before_changes = _get_loadflow_results(pfcgmes)
+    study_case_source.Activate()
+    for load in pfcgmes.get_obj('*.ElmLod'):
+        load.qlini = 0
+    voltages_source, active_powers_source = _get_loadflow_results(pfcgmes)
+    
+    _assert_that_steady_state_changed(voltages_source_before_changes, voltages_source)
+
+    pfcgmes.cgmes_export(OUTPUT_PATH, selected_profiles='ssh dl', as_zip=True)
+    study_case_destination.Activate()
+    base_archive = pfcgmes.get_unique_obj(r'cgmes_archive_folder\cgmes_archive_imported')
+    pfcgmes.update_profiles(
+        OUTPUT_PATH + '\\' + pfcgmes.EXPORTED_ZIP_NAME + '.zip',
+        base_archive    
+    )
+    voltages_destination, active_powers_destination = _get_loadflow_results(pfcgmes)
+    
+    for load in pfcgmes.get_obj('*.ElmLod'):
+        assert load.qlini == 0
+    _compare_loadflow_results(voltages_source, voltages_destination)
+    _compare_loadflow_results(active_powers_source, active_powers_destination)
 
 
 if __name__ == "__main__":
