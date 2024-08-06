8fca3b85fae20673d7c1c54be7c3734a72218678
Simon <simon.eberlein@gmx.de>
2024-08-06 10:09:34 +0200

    fix ensure that all data is saved in database before version is created

--- src/powfacpy/active_project_interface.py
+++ src/powfacpy/active_project_interface.py
@@ -490,6 +490,7 @@ class PFActiveProject(PFFolder):
         version = self.get_project_version(version_name)
         if version and overwrite:
             version.Delete()
+        self.app.WriteChangesToDb()    
         self._folder.CreateVersion(version_name)
         
     def rollback_project_to_previous_version(
