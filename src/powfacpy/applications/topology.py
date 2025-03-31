09acde79f26947b97b6e445bbf9800a13b27745a
Simon <simon.eberlein@gmx.de>
2025-03-31 18:11:51 +0200

    add create_area to topology (also fix some docstrings)

--- src/powfacpy/applications/topology.py
+++ src/powfacpy/applications/topology.py
@@ -2,9 +2,15 @@ from typing import Callable
 
 from icecream import ic
 
-from powfacpy.pf_class_protocols import PFApp, PFGeneral, ElmZone, ElmBoundary
 from powfacpy.pf_classes.elm.boundary import Boundary
 from powfacpy.applications.application_base import ApplicationBase
+from powfacpy.pf_classes.protocols import (
+    PFApp,
+    PFGeneral,
+    ElmZone,
+    ElmBoundary,
+    ElmArea,
+)
 
 
 class Topology(ApplicationBase):
@@ -33,6 +39,25 @@ class Topology(ApplicationBase):
             elm.cpZone = zone
         return zone
 
+    def create_area(
+        self,
+        name: str,
+        elms: list[PFGeneral],
+        color: int = 2,
+        parent_folder: str | PFGeneral = None,
+        overwrite: bool = True,
+    ) -> ElmZone:
+        if not parent_folder:
+            parent_folder = self.act_prj.areas_folder
+        area: ElmArea = self.act_prj.create_in_folder(
+            name + ".ElmArea", parent_folder, overwrite=overwrite
+        )
+
+        area.icolor = color
+        for elm in elms:
+            elm.cpArea = area
+        return area
+
     def create_boundary_using_intermediate_zone(
         self,
         name: str,
@@ -42,6 +67,25 @@ class Topology(ApplicationBase):
         parent_folder: PFGeneral | str | None = None,
         overwrite: bool = True,
     ) -> ElmBoundary:
+        """Create boundary (ElmBoundary) that includes 'elms'. A zone is created as an intermediate step.
+
+        Warning: If there are existing zones in the network model those might be changed if 'elms' are included (because elements can only be part of one zone). To avoid this use 'create_boundary_without_changing_initial_zones' instead.
+
+        Args:
+            name (str): Name of boundary created
+
+            elms (list[PFGeneral]): Network elements contained in boundary
+            exclude_node_elms (Callable | None, optional): Condition to select elements to be excluded from the interior (e.g. `lambda x: x.GetClassName() == "ElmLod" to exclude all ElmLod). Defaults to None.
+
+            color (int, optional): Color of boundary shown in single line diagram. Defaults to 2.
+
+            parent_folder (PFGeneral | str | None, optional): PArent folder where boundary is created. Defaults to None (i.e. default boundary folder).
+
+            overwrite (bool, optional): Overwrite existing boundary with same name. Defaults to True.
+
+        Returns:
+            ElmBoundary: Boundary object created.
+        """
         if parent_folder:
             parent_folder = self.act_prj._handle_single_pf_object_or_path_input(
                 parent_folder
@@ -75,6 +119,25 @@ class Topology(ApplicationBase):
         parent_folder: PFGeneral | str | None = None,
         overwrite: bool = True,
     ) -> ElmBoundary:
+        """Create boundary (ElmBoundary) that includes 'elms'. A zone is created as an intermediate step.
+
+        In comparison to 'create_boundary_using_intermediate_zone', this methods avoids altering existing zones by creating an intermediate variation.
+
+        Args:
+            name (str): Name of boundary created
+
+            elms (list[PFGeneral]): Network elements contained in boundary
+            exclude_node_elms (Callable | None, optional): Condition to select elements to be excluded from the interior (e.g. `lambda x: x.GetClassName() == "ElmLod" to exclude all ElmLod). Defaults to None.
+
+            color (int, optional): Color of boundary shown in single line diagram. Defaults to 2.
+
+            parent_folder (PFGeneral | str | None, optional): PArent folder where boundary is created. Defaults to None (i.e. default boundary folder).
+
+            overwrite (bool, optional): Overwrite existing boundary with same name. Defaults to True.
+
+        Returns:
+            ElmBoundary: Boundary object created.
+        """
         if parent_folder:
             parent_folder = self.act_prj._handle_single_pf_object_or_path_input(
                 parent_folder
