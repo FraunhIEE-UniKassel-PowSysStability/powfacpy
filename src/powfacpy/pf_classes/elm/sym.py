8e82979e90c7cd6be7f2a6f34a07a7db6da9aa20
Simon <simon.eberlein@gmx.de>
2026-01-16 12:02:55 +0100

    update SM interface

--- src/powfacpy/pf_classes/elm/sym.py
+++ src/powfacpy/pf_classes/elm/sym.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from powfacpy.base.active_project import ActiveProjectCached
-from powfacpy.pf_classes.protocols import ElmSym, TypSym, ElmTerm
+from fnmatch import fnmatch
 
+from powfacpy.base.active_project import ActiveProjectCached
+from powfacpy.pf_classes.protocols import ElmSym, PFGeneral, TypSym, ElmTerm
 from powfacpy.pf_classes.elm.elm_base import ElmBase, SinglePortBase
+from powfacpy.pf_classes.elm.term import Terminal
 from powfacpy.result_variables import ResVar
+
 
 LDF = ResVar.LF_Bal
 
@@ -68,7 +71,7 @@ class SynchronousMachine(ElmBase, SinglePortBase):
     def get_approximate_internal_voltage(self) -> complex:
         """Get approximate internal voltage from power supply, terminal voltage and internal reactance.
 
-        This is for example one way a system operator could approximate the internal voltage based on measurements at the point of connection.
+        This is one way a system operator could approximate the internal voltage based on measurements at the point of connection.
 
         Returns:
             complex: approximate internal voltage
@@ -86,6 +89,51 @@ class SynchronousMachine(ElmBase, SinglePortBase):
             return self._obj.typ_id.h
         else:
             return self._obj.typ_id.h * (self.ratedS / base_apparent_power_MVA)
+
+    def get_connecting_transformer(
+        self, transformer_class: str = "ElmTr*"
+    ) -> PFGeneral:
+        """Gets the next trafo (compared to GetStepupTransformer which requires a voltage level to stop the search)
+
+        Args:
+            transformer_class (str, optional): _description_. Defaults to "ElmTr2".
+
+        Returns:
+            PFGeneral: _description_
+        """
+        return Terminal(self.bus1.cterm).get_connected_elements(
+            condition=lambda x: fnmatch(x.GetClassName(), transformer_class)
+        )[0]
+
+    def get_terminal_of_transformer_hv_side(
+        self, transformer_class: str = "ElmTr*"
+    ) -> ElmTerm:
+        return self.get_connecting_transformer(transformer_class).bushv.cterm
+
+    def add_external_station_controller(
+        self,
+        parent_folder: PFGeneral | None = None,
+        controlled_terminal: ElmTerm | None = None,
+    ) -> None:
+        """Add external station controller.
+
+        Args:
+            parent_folder (PFGeneral | None, optional): Parent folder of station controller. Defaults to None (same folder as ElmSym is used).
+            controlled_terminal (ElmTerm | None, optional): target terminal. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if parent_folder is None:
+            parent_folder = self._obj.GetParent()
+        act_prj = ActiveProjectCached()
+        station_ctrl = act_prj.create_in_folder(
+            self._obj.loc_name + " station ctrl.ElmStactrl", parent_folder
+        )
+        self._obj.c_pstac = station_ctrl
+        if controlled_terminal is not None:
+            station_ctrl.rembar = controlled_terminal
+        return station_ctrl
 
     @staticmethod
     def get_cgmes_mapping():
